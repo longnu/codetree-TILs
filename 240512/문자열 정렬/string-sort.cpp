ca8acd9bf10cdfbf810079d9392e38ab818360a8
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-12 11:44:01 +0900

    Title: 문자열 정렬, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/문자열 정렬/string-sort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a;
+    cin >> a;
+    sort(a.begin(), a.end());
+    cout << a;
+    return 0;
+}
