8b6cfd788c9e39cf124a36d832ce56a450bbe933
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-12 11:35:23 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240512/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
+++ 240512/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 변수 선언
+    double a, b, c;
+
+	// 입력
+	cin >> a >> b >> c;
+	
+	cout << fixed;
+	cout.precision(3);
+    
+    // 출력
+    cout << a << endl;
+	cout << b << endl;
+	cout << c;
     return 0;
 }
