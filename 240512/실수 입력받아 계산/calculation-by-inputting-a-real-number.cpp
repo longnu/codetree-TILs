83a5cae7fa63c1982c645f966890878991ca6fed
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-12 11:34:56 +0900

    Title: 실수 입력받아 계산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    double a, b;
+    cin >> a >> b;
+
+    double answer = a + b;
+
+    
+    cout << fixed;
+    cout.precision(2);
+    cout << answer;
+
+
+    return 0;
+}
