bbfcae61e87e2274859c13418b131bfb7380c6c1
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-13 07:15:52 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240513/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int cnt;
+recur(int n)
+{
+    if(n==1)
+    {
+        return cnt;
+    }
+    if(n%2 == 0)
+    {
+        cnt++;
+        return recur(n/2); 
+    }
+    
+    if(n%2!=0)
+    {
+        cnt++;
+        return recur(n*3+1);
+    }
+}
+
+int main() {
+    
+    int n ;
+    cin >> n;
+
+    cout << recur(n);
+
+    return 0;
+}
