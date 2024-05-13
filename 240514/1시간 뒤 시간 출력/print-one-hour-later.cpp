b9cfcb91de70e27826feb67633baef648c0d5435
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-14 05:58:10 +0900

    Title: 1시간 뒤 시간 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240514/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int h;
+    char c;
+    int m;
+
+    cin >> h;
+    cin >> c;
+    cin >> m;
+
+    cout<<h+1<<c<<m;
+
+    return 0;
+}
