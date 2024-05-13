aee43f365fbadeb75ea9af916df4c3cdf126f838
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-14 06:06:12 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240514/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a, b;
+
+	// 입력
+	cin >> a;
+    cin.get();
+    cin >> b;
+    
+    // 출력
+    cout << a << b;
+    return 0;
+}
