c0bb921c7f2eb902690d562d032837773611dd62
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-15 08:58:32 +0900

    Title: 비오는 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240515/비오는 날/rainy-day.py
+++ 240515/비오는 날/rainy-day.py
@@ -1,24 +1,40 @@
-# 변수 선언 및 입력
-n = int(input())
+#include <iostream>
+#include <string>
 
+using namespace std;
 
-# Forecast 정보를 나타내는 클래스 선언
-class Forecast:
-    def __init__(self, date, day, weather):
-        self.date, self.day, self.weather = date, day, weather
+// Forecast 정보를 나타내는 클래스 선언
+class Forecast {
+    public:
+        string date, day, weather;
+        Forecast(string date, string day, string weather) {
+            this->date = date;
+            this->day = day;
+            this->weather = weather;
+        }
+};
 
+Forecast ans = Forecast("9999-99-99", "", "");
 
-ans = Forecast("9999-99-99", "", "")
-for _ in range(n):
-    date, day, weather = tuple(input().split())
+int main(){
+    // 변수 선언 및 입력
+    int n;
+    cin >> n;
 
-    # Forecast 객체를 만들어 줍니다.
-    f = Forecast(date, day, weather)
-    if weather == "Rain":
-        # 비가 오는 경우 가장 최근인지 확인하고,
-        # 가장 최근일 경우 정답을 업데이트합니다.
-        if ans.date >= f.date:
-            ans = f
+    for(int i = 1; i <= n; i++) {
+        string date, day, weather;
+        cin >> date >> day >> weather;
 
-# 결과를 출력합니다.
-print(ans.date, ans.day, ans.weather)
+        // Forecast 객체를 만들어 줍니다.
+        Forecast f = Forecast(date, day, weather);
+        if(weather == "Rain")
+            // 비가 오는 경우 가장 최근인지 확인하고,
+            // 가장 최근일 경우 정답을 업데이트합니다.
+            if(ans.date >= f.date)
+                ans = f;
+    }
+
+    // 결과를 출력합니다.
+    cout << ans.date << " " << ans.day << " " << ans.weather;
+    return 0;
+}
