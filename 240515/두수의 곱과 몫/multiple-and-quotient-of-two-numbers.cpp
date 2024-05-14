d1c207397bb2585ed7c7d925767d107a63345aaa
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-15 08:13:19 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240515/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a, b;
+
+	// 입력
+	cin >> a >> b;
+    
+    // 출력
+    cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / b;
+    return 0;
+}
