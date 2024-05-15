c94e56ebe8fe4da3433e626a79355324f72f1179
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-15 16:21:34 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240515/그 요일은/the-day-of-the-day.cpp
+++ 240515/그 요일은/the-day-of-the-day.cpp
@@ -1,22 +1,8 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-string day_arr[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
-int days_arr[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-int returnidx (string s) {
-    // 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
-    if(s == "Mon") return 0;
-    else if(s == "Tue") return 1;
-    else if(s == "Wed") return 2;
-    else if(s == "Thu") return 3;
-    else if(s == "Fri") return 4;
-    else if(s == "Sat") return 5;
-    return 6;
-}
-
-int daysofdate(int m, int d) {
+int NumOfDays(int m, int d) {
     // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
     int days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int total_days = 0;
@@ -30,32 +16,38 @@ int daysofdate(int m, int d) {
     
     return total_days;
 }
-int answer;
+
+int NumOfDay(string s) {
+    // 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
+    if(s == "Mon") return 0;
+    else if(s == "Tue") return 1;
+    else if(s == "Wed") return 2;
+    else if(s == "Thu") return 3;
+    else if(s == "Fri") return 4;
+    else if(s == "Sat") return 5;
+    return 6;
+}
+
+int ans;
+
 int main() {
-    // 여기에 코드를 작성해주세요.
-
-    int m1, d1, m2, d2;
+    // 변수 선언 및 입력
+    int m1, m2, d1, d2;
     string A;
-
     cin >> m1 >> d1 >> m2 >> d2;
-    cin >>A;
+    cin >> A;
 
-    int curidx = returnidx(A);
+    int start_date = NumOfDays(m1, d1);
+    int end_date = NumOfDays(m2, d2);
+    int cur_day = NumOfDay("Mon");
 
-    int daysoffirst = daysofdate(m1, d1);
-    int daysofsecond = daysofdate(m2, d2);
-
-    for(int date = daysoffirst; date <= daysofsecond; date++)
-    {
-        if(curidx == returnidx(A) )
-        {
-           answer++; 
-        }
-        curidx = (curidx+1)%7;
+    for(int date = start_date; date <= end_date; date++) {
+        // 오늘의 요일이 A요일과 같다면 정답에 추가합니다.
+        if(cur_day == NumOfDay(A)) ans++;
+        cur_day = (cur_day + 1) % 7;
     }
-    cout << answer;
-
-
-
+    
+    // 출력
+    cout << ans;
     return 0;
 }
