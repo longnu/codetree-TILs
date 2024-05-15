ff97a6e565b0e65d4ee609e0884931ab31df9ad7
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-15 16:20:48 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240515/그 요일은/the-day-of-the-day.cpp
+++ 240515/그 요일은/the-day-of-the-day.cpp
@@ -16,16 +16,19 @@ int returnidx (string s) {
     return 6;
 }
 
-int daysofdate(int month, int day)
-{
-    int days = 0;
-    for(int i = 1; i < month; i++)
-    {
-        days = days + days_arr[i];
-    }
-
-    days = days + day;
-    return days;
+int daysofdate(int m, int d) {
+    // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+    int days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = 0;
+    
+    // 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+    for(int i = 1; i < m; i++)
+        total_days += days[i];
+    
+    // m월의 경우에는 정확이 d일만 있습니다.
+    total_days += d;
+    
+    return total_days;
 }
 int answer;
 int main() {
