437fde7d6f109d61e9cec89d0b5469fe620c2a06
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-18 18:35:06 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240518/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n;
+int arr[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    int ans = 0; cnt = 0;
+    for(int i =0; i < n; i++){
+        if(i >= 1 && arr[i] == arr[i - 1]) cnt++;
+        else cnt = 1;
+        ans = max(ans, cnt);
+    }
+
+    cout << ans;
+
+    return 0;
+}
