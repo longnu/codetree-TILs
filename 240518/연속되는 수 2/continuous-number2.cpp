2b2ad371504ebe408f47e9eec55a10e190470146
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-18 18:35:25 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240518/연속되는 수 2/continuous-number2.cpp
+++ 240518/연속되는 수 2/continuous-number2.cpp
@@ -13,7 +13,7 @@ int main() {
     cin >> n;
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    int ans = 0; cnt = 0;
+    int ans = 0; int cnt = 0;
     for(int i =0; i < n; i++){
         if(i >= 1 && arr[i] == arr[i - 1]) cnt++;
         else cnt = 1;
