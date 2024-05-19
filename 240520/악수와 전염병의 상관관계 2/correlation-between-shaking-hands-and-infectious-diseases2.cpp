fb9c30ac679582a3261b4d5107e2806409cf4bb6
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-20 04:48:35 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240520/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240520/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -15,57 +15,55 @@ class Shake{
         int time;
         int person1;
         int person2;
-        Shake(int time, int person1, int person2){
+        Shake(int time, int person1, int person2) {
             this->time = time;
             this->person1 = person1;
             this->person2 = person2;
         }
-        Shake(){}
+		Shake(){}
 };
 
-bool Cmp(const Shake &a, const Shake &b){
+bool Cmp(const Shake &a, const Shake &b) {
     return a.time < b.time;
 }
 
 int main() {
+    // 입력
     cin >> n >> k >> p >> t;
-    infected[p] = true;
-
-    Shake shakes[MAX_T];
-
-    for(int i = 0; i < t; i++)
-    {
-        int time, person1, person2;
+	infected[p] = true;
+	
+	Shake shakes[MAX_T];
+	
+    for(int i = 0; i < t; i++) {
+		int time, person1, person2;
         cin >> time >> person1 >> person2;
-        shakes[i] = Shake(time, person1, person2);
-    }
-
+		shakes[i] = Shake(time, person1, person2);
+	}
+	
     sort(shakes, shakes + t, Cmp);
-
-    for(int i = 0; i < t; i++)
-    {
+    
+    for(int i = 0; i < t; i++) {
         int target1 = shakes[i].person1;
-        int target2 = shakes[i].person2;
-
-        if(infected[target1]) 
-            shake_num[target1]++;
-        if(infected[target2])
-            shake_num[target2]++;
-        if(infected[target1] && shake_num[target1] <= k)
-        {
-            infected[target2] = true;
-        }
-        if(infected[target2] && infected[target2])
-        {
-            infected[target1] = true;
-        }
+		int target2 = shakes[i].person2;
+		
+        if(infected[target1])
+			shake_num[target1]++;
+		if(infected[target2])
+			shake_num[target2]++;
+		
+		if(shake_num[target1] <= k && infected[target1])
+			infected[target2] = true;
+		
+		if(shake_num[target2] <= k && infected[target2])
+			infected[target1] = true;
     }
-
-    for(int i = 1; i <=n; i++)
-    {
-        if(infected[i]) cout << 1;
-        else cout << 0;
-    }
-
+    
+    for(int i = 1; i <= n; i++) {
+		if(infected[i])
+			cout << 1;
+		else
+			cout << 0;
+	}
+	
     return 0;
 }
