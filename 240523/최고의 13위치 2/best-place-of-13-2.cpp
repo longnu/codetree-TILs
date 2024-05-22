219a4b04551e3e266e381486a89667ab26ab0925
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-23 07:18:05 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240523/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 20
+int n;
+int arr[MAX_N][MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int answer = 0;
+    int number = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= n-2; j++)
+        {
+            for(int k = i+1; k < n; k++)
+            {
+                for(int l = 0; l<= n-2; l++)
+                {
+                    number = arr[i][j] + arr[i][j+1]+arr[i][j+2] + arr[k][l] + arr[k][l+1]+arr[k][l+2];
+                    answer = max(answer, number);
+                }
+            }
+        }
+    }
+    cout << answer;
+
+    return 0;
+}
