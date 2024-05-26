6f1671bdf60175428cb940df96ad916b29a9b75c
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-27 04:25:11 +0900

    Title: 두 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240527/두 직사각형/two-rectangles.cpp
+++ 240527/두 직사각형/two-rectangles.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int x1, x2, y1, y2, a1, a2, b1, b2;
-    cin >> x1 >> x2 >> y1 >> y2 >> a1 >> a2 >> b1 >> b2;
+    cin >> x1 >> y1 >> x2 >> y2 >> a1 >> b1 >> a2 >> b2;
 
     if(x1 > a2 || a1 > x2 || y1 > b2 || b1 > y2)
     {
