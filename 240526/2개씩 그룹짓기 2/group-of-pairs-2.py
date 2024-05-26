39371d5b06794725894b902e094770d507dd4e03
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-26 15:05:59 +0900

    Title: 2개씩 그룹짓기 2, Time: 182ms, Memory: 40MB, Status: Passed - CodeTree

--- 240526/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240526/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -1,29 +1,17 @@
-# n개의 그룹으로 만들어
-# 각 그룹내의 2개의 수의 차이 중
-# 최솟값이 최대가 되도록 하는
-
 import sys
 
-si = sys.stdin.readline
+INT_MAX = sys.maxsize
 
-# 1 <= n <= 100,000 -> n^2 만 해도 100억 이중 포문 풀이로 불가능
-n = int(si())
-
-arr = [0] + list(map(int, si().split()))
-
-# 2n 개의 원소를 n개의 그룹으로 쪼개기 
-# 두 원소의 차이의 최솟값을 최대로 하기 위해선...
-# 차이의 최솟값을 최대..
+# 변수 선언 및 입력:
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+ans = INT_MAX
 
 arr.sort()
 
-# print(arr)
-# 2 5 7 9 10 15
-
-# (1, n + 1) (2, n + 2) ... 이런식으로 짝지으면 되나?
-
-min_diff = sys.maxsize 
+# 정렬한 뒤 1번째 값과 n + 1번째 값, 2번째 값과 n + 2번째 값, ... n번째 값과 2n번째 값의
+# 차이를 구합니다. 구한 값들의 최솟값을 찾습니다.
 for i in range(1, n + 1):
-    min_diff = min(min_diff, arr[n + i] - arr[i])
+    ans = min(ans, arr[n + i] - arr[i])
 
-print(min_diff)
+print(ans)
