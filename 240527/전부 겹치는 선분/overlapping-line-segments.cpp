78c4b519af7f0b5a172ad78b6cb9ed5323d66619
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-27 04:41:42 +0900

    Title: 전부 겹치는 선분, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240527/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 240527/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
-#define MAX_N 100
-int arr[MAX_N + 1];
+
+int n;
+int x1, x2;
+int max_x1;
+int min_x2 = INT_MAX;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n = 0;
+    // 입력
     cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        int x1, x2;
+
+    // 시작점 중 가장 뒤에 있는 좌표와 끝점 중 가장 앞에 있는 점의 좌표를 확인합니다.
+    for(int i = 0; i < n; i++) {
         cin >> x1 >> x2;
-        for(int i = x1; i <= x2; i++)
-        {
-            arr[i]++;
-        }
+        max_x1 = max(max_x1, x1);
+        min_x2 = min(min_x2, x2);
     }
-    int pass = false;
-    for(int i = 0; i < MAX_N+1; i ++)
-    {
-        if(arr[i] == n)
-        {
-            pass = true;
-            break;
-        }
-    }
-    if(pass == true)
-    {
+    
+    // 만약 어느 한 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 온다면
+    // 선분이 전부 겹칠 수 없습니다.
+    if(min_x2 >= max_x1)
         cout << "Yes";
-    }
     else
-    {
         cout << "No";
-    }
+
     return 0;
 }
