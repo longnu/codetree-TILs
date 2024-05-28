8211b289bafb9718538c24241cccfdb2fa6246fd
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-05-28 22:26:26 +0900

    Title: 스승의 은혜 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240528/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240528/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,28 +1,55 @@
-n, b = map(int, input().split())
-wishes = [tuple(map(int, input().split())) for _ in range(n)]
+#include <iostream>
+#include <algorithm>
 
-ans = 0
-for i in range(n):
-    # 학생들이 원하는 선물의 가격 리스트
-    tmp = [list(wishes[j]) for j in range(n)]
+#define MAX_N 1000
 
-    # 선물 한 개를 반값 할인
-    tmp[i][0] /= 2
+using namespace std;
 
-    # (선물 가격 + 배송비) 리스트
-    prices = [(tmp[k][0] + tmp[k][1]) for k in range(n)]
-    prices.sort()
+int n, b;
+int p[MAX_N];
+int s[MAX_N];
 
-    # 선물할 수 있는 학생수, 현재까지 쓴 예산
-    student = 0
-    cnt = 0
+int main() {
+    // 입력
+    cin >> n >> b;
 
-    for x in range(n):
-        if cnt + prices[x] > b:
-            break
-        cnt += prices[x]
-        student += 1
+    for(int i = 0; i < n; i++)
+        cin >> p[i] >> s[i];
 
-    ans = max(ans, student)
+    int ans = 0;
+    
+    // 한 명의 학생에 선물 쿠폰을 쓸 때 선물 가능한 학생의 최대 명수를 구합니다.
+    for(int i = 0; i < n; i++) {
+        // i번째 학생의 선물에 쿠폰을 쓸 때 선물 가능한 학생 수를 구합니다.
 
-print(ans)
+        // tmp배열을 만들어 i번째 학생의 선물에 쿠폰을 쓸 때
+        // 각 학생의 원하는 선물 가격을 저장합니다.
+        int tmp[MAX_N] = {};
+        for(int j = 0; j < n; j++)
+            tmp[j] = p[j] + s[j];
+        tmp[i] = p[i] / 2 + s[i];
+
+        // 학생을 선물 가격 순으로 정렬합니다.
+        // 선물 가격이 가장 작은 학생부터 선물을 사 줄 때,
+        // 반드시 가장 많은 학생에게 선물을 줄 수 있습니다.
+        sort(tmp, tmp + n);
+
+        int student = 0;
+        int cnt = 0;
+
+        // 가장 많은 학생에게 선물을 줄 때, 그 학생 수를 구합니다.
+        // student : 선물받은 학생 수 / cnt : 현재까지 쓴 돈
+        for(int j = 0; j < n; j++) {
+            if(cnt + tmp[j] > b)
+                break;
+            cnt += tmp[j];
+            student++;
+        }
+
+        ans = max(ans, student);
+    }
+
+    cout << ans;
+    
+    return 0;
+}
