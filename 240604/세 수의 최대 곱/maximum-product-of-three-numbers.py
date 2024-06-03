2026dffdf0795dbf48e55d910153505e294670fe
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-06-04 08:45:53 +0900

    Title: 세 수의 최대 곱, Time: 140ms, Memory: 31MB, Status: Passed - CodeTree

--- 240604/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240604/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,18 +1,40 @@
 import sys
+
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력:
 n = int(input())
-nums = list(map(int, sys.stdin.readline().split()))
+arr = [INT_MIN] + list(map(int, input().split()))
 
-nums.sort()
-temp = 1
-temp2 = 1
-# 양수 3개 곱하기
-for i in range(1, 4):
-    temp *= nums[-i]
+ans = 0
+negative, zero, positive = 0, 0, 0
 
-# 음수 2개 양수 1개 곱하기
-for i in range(2):
-    temp2 *= nums[i]
-temp2 *= nums[-1]
+arr.sort()
+for i in range(1, n + 1):
+    if arr[i] < 0: 
+        negative += 1
+    if arr[i] == 0: 
+        zero += 1
+    if arr[i] > 0: 
+        positive += 1
 
-# 두 값을 비교하기
-print(max(temp, temp2))
+# 곱 중 양수가 존재할 때
+# 양수 3개의 곱이나, 양수 1개와 음수 2개의 곱이 만들어져야 가능합니다.
+if positive >= 3 or (positive >= 1 and negative >= 2):
+    # 양수가 3개 이상이라면, 그 중 가장 큰 3개의 수를 곱하는 것이 최선입니다.
+    if positive >= 3:
+        ans = max(ans, arr[n] * arr[n - 1] * arr[n - 2])
+    # 음수 2개와 양수 1개를 곱할 때에는, 음수 2개는 절댓값이 가장 큰 값을, (즉, 가장 작은 두 값)
+    # 양수 1개는 가장 큰 값을 골라 곱하는 것이 최선입니다.
+    if positive >= 1 and negative >= 2:
+        ans = max(ans, arr[n] * arr[2] * arr[1])
+# 곱 중 0이 존재할 때
+elif zero >= 1:
+    ans = 0
+# 곱 중 음수만 존재할 때
+# 배열에 -밖에 없거나 (negative = 1, zero = 0, positive = 2)인 경우
+# 이 경우 가장 절댓값이 작은 값 3개를 고르는 것이 최선입니다.
+else:
+    ans = arr[n] * arr[n - 1] * arr[n - 2]
+
+print(ans)
