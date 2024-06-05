bae025aa59b3a19f88d12738977d38f1eb5acbc4
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-06-05 09:38:33 +0900

    Title: X 달리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240605/X 달리기/x-run.py
@@ -0,0 +1,27 @@
+Dislike
+X = int(input())
+
+curr_pos = 0
+curr_speed = 1
+sum_time = 0
+while (True):
+    curr_pos += curr_speed
+    sum_time += 1
+    curr_speed += 1
+    increase_sum = 0
+
+    if (curr_pos == X):
+        break
+    
+    for i in range(curr_speed):
+        increase_sum += i
+    if X - (curr_pos + curr_speed) < increase_sum:
+        curr_speed -= 1
+    
+    stay_sum = 0
+    for i in range(curr_speed):
+        stay_sum += i
+    if X - (curr_pos + curr_speed) < stay_sum:
+        curr_speed -= 1
+
+print(sum_time)
