ab6388ebe78bd4a5d6c7736da19b8903743e348d
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-06-06 04:50:42 +0900

    Title: ABC 줄 세우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240606/ABC 줄 세우기/abc-line-up.py
+++ 240606/ABC 줄 세우기/abc-line-up.py
@@ -1,29 +1,35 @@
-N = int(input())
-people = [
-    ord(ele) - ord('A') for ele in input().split()
-]
+#include <iostream>
 
-count = 0
-# 길이가 1이면 움직일 필요 없음
-if len(people) == 1:
-    print(0)
-# 둘 이상일 때
-else:
-    # 각 위치에 올바로 서있지 않으면 올바른 위치로 옮겨줌
-    for i in range(N):
-        order_index = people.index(i)
-        temp = people[order_index]
-        # order_index가 서있는 위치가 있어야 할 위치(i)보다 뒤에 있는 경우
-        if order_index > i:
-            for j in range(order_index, i, -1):
-                people[j] = people[j-1]
-            people[i] = temp
-            count += abs(order_index - i)
-        # order_index가 서있는 위치가 있어야 할 위치(i)보다 앞에 있는 경우
-        elif order_index < i:
-            for j in range(order_index, i-1):
-                people[j+1] = people[j]
-            people[i] = temp
-            count += abs(order_index - i)
+#define MAX_N 26
 
-print(count)
+using namespace std;
+
+int n;
+char arr[MAX_N + 1];
+int ans;
+
+int main() {
+    // 입력
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        cin >> arr[i];
+
+    // 'A' 부터 알파벳 순서대로 찾아 가장 앞으로 이동합니다.
+    for(int i = 1; i <= n; i++) {
+        char x = 'A' + i - 1;
+
+        // i번째 알파벳을 찾아 idx에 저장합니다.
+        int idx = 0;
+        for(int j = 1; j <= n; j++)
+            if(arr[j] == x) idx = j;
+        
+        // idx번째 알파벳을 i번째 위치까지 swap합니다.
+        for(int j = idx - 1; j >= i; j--) {
+            swap(arr[j], arr[j + 1]);
+            ans++;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
