430a2efcb990207c522533e78edf6345165c6543
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-06-21 08:02:05 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240621/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int dp[MAX_NUM + 1];
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 1;
+    for(int i= 3; i <= n; i++)
+    {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout << dp[n];
+    return 0;
+}
