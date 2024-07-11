458761b133633740a140d6749187d0b922d8db05
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-07-11 09:04:13 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240711/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
+++ 240711/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -1,99 +1,86 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define MAX_N 20
 #define DIR_NUM 4
 
 int grid[MAX_N][MAX_N];
-int nextgrid[MAX_N][MAX_N];
+int next_count[MAX_N][MAX_N];
 int count[MAX_N][MAX_N];
-int n,m,t;
+int n, m, t;
 
-int dx[DIR_NUM] = {-1, 1, 0, 0};
+int dx[DIR_NUM] = {-1, 1, 0, 0}; // 상, 하, 좌, 우
 int dy[DIR_NUM] = {0, 0, -1, 1};
 
-bool InRange(int x, int y)
-{
-    return x >=0 && x < n && y >= 0 && y < n;
+bool InRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-
     cin >> n >> m >> t;
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
             cin >> grid[i][j];
         }
     }
 
-    for(int i =0; i < m; i++)
-    {
+    for(int i = 0; i < m; i++) {
         int r, c;
         cin >> r >> c;
-        count[r-1][c-1] = 1;
+        count[r-1][c-1] = 1; // 구슬의 시작 위치 설정
     }
 
-    for(int i =0; i < t; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            for(int k = 0; k < n; k++)
-            {
-                if(count[j][k])
-                {
+    for(int time = 0; time < t; time++) {
+        // 이동 후 새로운 위치에 대한 카운트를 초기화
+        fill(&next_count[0][0], &next_count[0][0] + sizeof(next_count) / sizeof(int), 0);
+        
+        // 모든 구슬의 위치를 확인하고 이동
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < n; k++) {
+                if(count[j][k]) {
                     int maxnum = -1;
-                    int max_nj = -1, max_nk = -1;
-
-                    for(int l = 0; l < DIR_NUM; l++)
-                    {
+                    int max_nj = j, max_nk = k; // 디폴트로 현재 위치
+                    
+                    for(int l = 0; l < DIR_NUM; l++) {
                         int nj = j + dx[l];
                         int nk = k + dy[l];
-                        if(InRange(nj, nk))
-                        {
-                            if(grid[nj][nk] > maxnum)
-                            {
+                        if(InRange(nj, nk)) {
+                            if(grid[nj][nk] > maxnum) {
+                                maxnum = grid[nj][nk];
                                 max_nj = nj;
                                 max_nk = nk;
                             }
                         }
                     }
-
-                    count[j][k]--;
-                    count[max_nj][max_nk]++;
+                    
+                    next_count[max_nj][max_nk] += count[j][k];
                 }
             }
         }
-
-        for(int j = 0; j < n; j++)
-        {
-            for(int k = 0; k < n; k++)
-            {
-                if(count[j][k] > 1)
-                {
-                    count[j][k] = 0;
+        
+        // 다음 시간의 위치 업데이트 및 충돌 제거
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < n; k++) {
+                if(next_count[j][k] > 1) {
+                    next_count[j][k] = 0;
                 }
+                count[j][k] = next_count[j][k];
             }
         }
     }
+
     int cnt = 0;
-    for(int j = 0; j < n; j++)
-        {
-            for(int k = 0; k < n; k++)
-            {
-                if(count[j][k] == 1)
-                {
-                    cnt++;
-                }
+    for(int j = 0; j < n; j++) {
+        for(int k = 0; k < n; k++) {
+            if(count[j][k] > 0) {
+                cnt++;
             }
         }
+    }
 
     cout << cnt;
-
-
 
     return 0;
 }
