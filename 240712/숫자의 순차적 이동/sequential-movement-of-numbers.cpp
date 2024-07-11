bc2bb08c3f34a4b80789a5fb8206d33c80106c1b
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-07-12 07:38:41 +0900

    Title: 숫자의 순차적 이동, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/숫자의 순차적 이동/sequential-movement-of-numbers.cpp
+++ 240712/숫자의 순차적 이동/sequential-movement-of-numbers.cpp
@@ -1,102 +1,84 @@
 #include <iostream>
-using namespace std;
+#include <tuple>
+
 #define MAX_N 20
-#include <utility>
-
-int n, m;
-
-int grid[MAX_N][MAX_N];
-
 #define DIR_NUM 8
 
-int dx[DIR_NUM] = {-1, -1, -1, 0, 1, 1, 1, 0};
-int dy[DIR_NUM] = {-1, 0, 1, 1, 1, 0, -1, -1};
+using namespace std;
 
-pair<int, int> searchnum(int num)
-{
+int n, m;
+int grid[MAX_N][MAX_N];
+
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+pair<int, int> FindPos(int num) {
     for(int i = 0; i < n; i++)
-    {
         for(int j = 0; j < n; j++)
-        {
             if(grid[i][j] == num)
-            {
                 return make_pair(i, j);
-            }
+}
+
+// 그 다음 위치를 찾아 반환합니다.
+pair<int, int> NextPos(pair<int, int> pos) {
+    int dx[DIR_NUM] = {-1, -1, -1,  0, 0,  1, 1, 1};
+    int dy[DIR_NUM] = { -1, 0,  1, -1, 1, -1, 0, 1};
+    
+    int x, y;
+    tie(x, y) = pos;
+    
+    // 인접한 8개의 칸 중 가장 값이 큰 위치를 찾아 반환합니다.
+    int max_val = -1;
+    pair<int, int> max_pos;
+    for(int i = 0; i < 8; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if(InRange(nx, ny) && grid[nx][ny] > max_val) {
+            max_val = grid[nx][ny];
+            max_pos = make_pair(nx, ny);
         }
     }
-    // num을 찾지 못한 경우 (-1, -1)을 반환
-    return make_pair(-1, -1);
+    
+    return max_pos;
 }
 
-bool InRange(int x, int y)
-{
-    return x >= 0 && x < n && y >= 0 && y < n;
+void Swap(pair<int, int> pos, pair<int, int> next_pos) {
+    int x, y, nx, ny;
+    tie(x, y) = pos;
+    tie(nx, ny) = next_pos;
+
+    int temp = grid[x][y];
+    grid[x][y] = grid[nx][ny];
+    grid[nx][ny] = temp;
 }
 
-pair<int, int> searchlow(int x, int y)
-{
-    int highr = 0;
-    int highc = 0;
-    int highnum = 0;
-    for(int i =0; i < DIR_NUM; i++)
-    {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-
-        if(InRange(nx, ny))
-        {
-            if(highnum < grid[nx][ny])
-            {
-                highr = nx;
-                highc = ny;
-                highnum = grid[nx][ny];
-            }
-        }
+void Simulate() {
+    // 번호가 증가하는 순으로
+    // 그 다음 위치를 구해
+    // 한 칸씩 움직입니다.
+    for(int num = 1; num <= n * n; num++) {
+        pair<int, int> pos = FindPos(num);
+        pair<int, int> next_pos = NextPos(pos);
+        Swap(pos, next_pos);
     }
-
-    return make_pair(highr, highc);
-     
 }
-
-
-
-void swapnum(pair<int, int> pos1, pair<int, int> pos2) {
-    int temp = grid[pos1.first][pos1.second];
-    grid[pos1.first][pos1.second] = grid[pos2.first][pos2.second];
-    grid[pos2.first][pos2.second] = temp;
-}
-
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin >> n >> m;
-
-    for(int i =0; i < n; i ++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            cin >>grid[i][j];
-        }
-    }
-
-    for(int i = 0; i < m; i++)
-    {
-        for(int num = 1; num <= n*n; num++)
-        {
-            pair<int, int> target = searchnum(num);
-            pair<int, int> lowest = searchlow(target.first, target.second);
-            swapnum(target, lowest);
-        }
-    }
-
-    for(int i =0; i < n; i ++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            cout << grid[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
+	cin >> n >> m;
+		
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			cin >> grid[i][j];
+	
+    // m번 시뮬레이션을 진행합니다.
+	while(m--)
+		Simulate();
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++)
+			cout << grid[i][j] << " ";
+		cout << endl;
+	}
+	
+	return 0;
 }
