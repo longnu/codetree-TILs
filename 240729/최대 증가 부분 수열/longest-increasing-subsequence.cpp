f04cf6d7fd3c6cdb1427c5a405906f8dcb3ba185
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-07-29 08:37:44 +0900

    Title: 최대 증가 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240729/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240729/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -25,7 +25,7 @@ int main() {
     for(int i = 1; i < n; i++) {
         for(int j = 0; j < i; j++) {
             if(a[i] > a[j]) {
-                dp[i] = max(dp[i], dp[j] + 1);
+                dp[i] = dp[j] + 1;
             }
         }
     }
