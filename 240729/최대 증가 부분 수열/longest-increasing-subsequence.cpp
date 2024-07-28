8dd15f7364e14b981b51f35f23442507329c53ea
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-07-29 08:32:59 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240729/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -1,47 +1,42 @@
 #include <iostream>
-#include <climits>
+#include <vector>
+#include <algorithm>
 using namespace std;
-#define MAX_N 1000
 
+#define MAX_N 1000
 
 int a[MAX_N];
 int dp[MAX_N];
 int n;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 입력 받기
     cin >> n;
-
-    for(int i = 0;i < n; i ++)
-    {
+    for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    for(int i = 0;i < n; i ++)
-    {
+    // dp 배열 초기화
+    for(int i = 0; i < n; i++) {
         dp[i] = 1;
     }
 
-    dp[0] = 1;
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 0; j < i; j++)
-        {
-            if(a[i] > a[j])
-            {
-                dp[i] = dp[j]+1;
+    // dp 계산
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(a[i] > a[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }
 
+    // 가장 긴 증가 부분 수열의 길이 구하기
     int ans = 0;
-    for(int i = 0 ; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         ans = max(ans, dp[i]);
     }
 
-    cout << ans;
-
+    cout << ans << endl;
 
     return 0;
 }
