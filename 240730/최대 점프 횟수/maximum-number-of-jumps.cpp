8ebf5688af999467deccbbfb3839e1a710e03f2b
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-07-30 04:52:19 +0900

    Title: 최대 점프 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/최대 점프 횟수/maximum-number-of-jumps.cpp
+++ 240730/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -1,42 +1,36 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
-#define MAX_N 1000
-
 int n;
+#define MAX_N 1000
 int a[MAX_N];
 int dp[MAX_N];
+#include <climits>
+
 
 int main() {
-    // 입력 받기
+    // 여기에 코드를 작성해주세요.
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
         cin >> a[i];
-
-    // DP 배열 초기화
-    for (int i = 0; i < n; i++)
-        dp[i] = -1;
+    for(int i =0; i < n; i++)
+        dp[i] = INT_MIN;
     dp[0] = 0;
-
-    // DP 계산
-    for (int i = 0; i < n; i++) {
-        if (dp[i] == -1) continue;  // 해당 위치에 도달할 수 없는 경우
-        for (int j = 1; j <= a[i]; j++) {
-            if (i + j < n) {
-                dp[i + j] = max(dp[i + j], dp[i] + 1);
+    for(int i = 1; i < n; i++)
+        for(int j = 0; j < i; j++)
+        {
+            if(dp[j] == INT_MIN)
+                continue;
+            if(j+a[j] >= i)
+            {
+                dp[i] = max(dp[i], dp[j]+1);
             }
         }
-    }
-
-    // 결과 찾기
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
+    int ans =0 ;
+    for(int i =0; i < n; i ++)
+    {
         ans = max(ans, dp[i]);
     }
-
-    // 결과 출력
-    cout << ans << endl;
+    cout << ans;
 
     return 0;
 }
