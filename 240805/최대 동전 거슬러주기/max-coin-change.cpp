e43aa73d6769f652d7822894563717d40f66d78d
SeongminNa <83560500+longnu@users.noreply.github.com>
2024-08-05 09:17:50 +0900

    Title: 최대 동전 거슬러주기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/최대 동전 거슬러주기/max-coin-change.cpp
+++ 240805/최대 동전 거슬러주기/max-coin-change.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int n, m;
 
 #define MAX_N 100
 #define MAX_M 10000
 
+int n, m;
 int coin[MAX_N];
 int dp[MAX_M+1];
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     cin >> n >> m;
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         cin >> coin[i];
     }
-    dp[0] = 0;
+    
+    fill(dp, dp + MAX_M + 1, -1); // dp 배열을 -1로 초기화
+    dp[0] = 0; // 0원을 만들기 위해 필요한 동전의 수는 0개
 
-    for(int i  =1; i <= m; i ++ )
-    {
-        for(int j = 0; j < n; j++)
-        {
-            if(i - coin[j] >= 0 )
-            {
-                dp[i] = max(dp[i], dp[i - coin[j]]+1);
+    for(int i = 1; i <= m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i - coin[j] >= 0 && dp[i - coin[j]] != -1) {
+                dp[i] = max(dp[i], dp[i - coin[j]] + 1);
             }
-            
         }
     }
-    cout << dp[m];
+
+    cout << dp[m] << endl;
     return 0;
 }
